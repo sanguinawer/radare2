43ace47121fda4de1af030ed846e636751c768b4
SkUaTeR <skuater@hotmail.com>
2016-11-17 12:09:37 +0100

    Update anal.c

--- libr/core/anal.c
+++ libr/core/anal.c
@@ -117,7 +117,7 @@ R_API ut64 r_core_anal_address(RCore *core, ut64 addr) {
 		RListIter *iter;
 		r_list_foreach (rs->regs, iter, r) {
 			if (r->type == R_REG_TYPE_GPR) {
-				ut64 val = r_reg_getv(core->dbg->reg, r->name);
+				ut64 val = r_reg_getv (core->dbg->reg, r->name);
 				if (addr == val) {
 					types |= R_ANAL_ADDR_TYPE_REG;
 					break;
